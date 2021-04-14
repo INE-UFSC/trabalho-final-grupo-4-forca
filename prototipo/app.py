abb447cf7877daf21e74454b4544d4245e05219e
EduardoC1705 <78425310+EduardoC1705@users.noreply.github.com>
2021-04-14 16:48:23 -0400

    menu + movimento

--- prototipo/app.py
+++ prototipo/app.py
@@ -1,29 +1,52 @@
 from cenas import *
 
 cenaAtual = menuPrincipal
-
+p = None
+cenarioAtual = "menu"
 #   Loop que mantém o jogo aberto.
 while jogoAberto:
+    cenaAtual.tecla = "outra"
 
 #   Verificação constante de eventos.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             jogoAberto = False
-        elif event.type == pygame.KEYDOWN:
+        elif cenarioAtual == "menu" and event.type == pygame.KEYDOWN:
             if event.key == pygame.K_s or event.key == pygame.K_DOWN:
                 cenaAtual.tecla = "baixo"
             elif event.key == pygame.K_w or event.key == pygame.K_UP:
                 cenaAtual.tecla = "cima"
-            else:
-                cenaAtual.tecla = "outra"
+            elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                cenaAtual.tecla = "esquerda"
+            elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                cenaAtual.tecla = "direita"
+            elif event.key == pygame.K_RETURN:
+                cenaAtual.tecla = "enter"
 
-            cenaAtual.eventos()
+    if cenarioAtual == "jogo":
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_s] or pressed[pygame.K_DOWN]:
+            cenaAtual.tecla = "baixo"
+        elif pressed[pygame.K_w] or pressed[pygame.K_UP]:
+            cenaAtual.tecla = "cima"
+        elif pressed[pygame.K_a] or pressed[pygame.K_LEFT]:
+            cenaAtual.tecla = "esquerda"
+        elif pressed[pygame.K_d] or pressed[pygame.K_RIGHT]:
+            cenaAtual.tecla = "direita"
+        elif pressed[pygame.K_RETURN]:
+            cenaAtual.tecla = "enter"
+                       
+
+    p = cenaAtual.eventos()
 
     cenaAtual.iniciar()
     cenaAtual.atualizar()
 
 #   Atualizar os elementos na tela.
     pygame.display.flip()
+    if p == "cenarioteste":
+        cenaAtual = cenarioteste
+        cenarioAtual = "jogo"
 
 #   Limitar o FPS do jogo.
     clock.tick(60)
