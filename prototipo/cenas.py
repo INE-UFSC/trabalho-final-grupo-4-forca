f6667479a18198b5ae5eb423ec4c828bc5c8725e
EduardoC1705 <78425310+EduardoC1705@users.noreply.github.com>
2021-04-21 13:41:02 -0400

    inimigo no cenario

--- prototipo/cenas.py
+++ prototipo/cenas.py
@@ -1,6 +1,7 @@
 from menu import *
 from variaveisGlobais import *
 from jogador import *
+from controladorInimigo import *
 
 
 class Cena:
@@ -73,33 +74,40 @@ class Saguao(Cena):
         self.altura = altura
 
 
-class CenarioTeste(Cena):
 
+
+class CenarioTeste(Cena):
     def __init__(self):
         super().__init__()
         self.fundo = pygame.Surface((self.largura, self.altura))
         self.parede = pygame.Surface((200,100))
         self.parede.fill(vermelho)
-        self.parede0rect = self.parede.get_rect(topleft=(100, 100))
-        self.parede1rect = self.parede.get_rect(topleft=(100, 300))
-        self.parede2rect = self.parede.get_rect(topleft=(100, 500))
+        self.parede0rect = self.parede.get_rect(topleft = (100, 100))
+        self.parede1rect = self.parede.get_rect(topleft = (100, 300))
+        self.parede2rect = self.parede.get_rect(topleft = (100, 500))
         self.lista_parederect = [self.parede0rect, self.parede1rect, self.parede2rect]
     
     def iniciar(self):
-        tela.blit(self.fundo, (0, 0))
-        tela.blit(self.parede, (100, 100))
+        tela.blit(self.fundo, (0,0))
+        tela.blit(self.parede, (100,100))
         tela.blit(self.parede, (100, 300))
         tela.blit(self.parede, (100, 500))
         tela.blit(jogador.surf, jogador.rect)
+        tela.blit(inimigo.surf, inimigo.rect)
     
     def eventos(self):
         jogador.move(self.tecla)
         for retangulo in self.lista_parederect:
             if jogador.rect.colliderect(retangulo):
                 jogador.resgata_posicao()
-
-    def atualizar(self):
-        pass
+        if jogador.rect.colliderect(inimigo.rect):
+            jogador.resgata_posicao()
+        controlador.movimenta()
+        for retangulo in self.lista_parederect:
+            if inimigo.rect.colliderect(retangulo):
+                inimigo.resgata_posicao()
+        if inimigo.rect.colliderect(jogador.rect):
+            inimigo.resgata_posicao()
 
 
 cena = Cena()
